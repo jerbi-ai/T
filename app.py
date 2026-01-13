418bb82934453c1606bca61e41d39e845e64abdb
jerbi-ai <jerbimohamedhedi@gmail.com>
2026-01-13 16:33:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
+# bonjour.py
 import streamlit as st
-import datetime
-from pytz import timezone
-from streamlit_autorefresh import st_autorefresh
 
-st.set_page_config(page_title="Horloge Tunisie", layout="centered")
-st.title("🕒 Heure en Temps Réel - Tunisie")
-
-# Rafraîchissement automatique toutes les 1000 ms
-st_autorefresh(interval=1000, key="clock")
-
-tunis_tz = timezone("Africa/Tunis")
-now = datetime.datetime.now(tunis_tz)
-current_time = now.strftime("%H:%M:%S")
-current_date = now.strftime("%A %d %B %Y")
-
-st.markdown(f"### {current_date}\n## {current_time}")
+st.title("Bonjour")  # Affiche un gros titre
+st.write("Bonjour ! 😊")  # Affiche un texte simple
