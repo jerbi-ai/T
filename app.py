046b83589437f8693d1d6a2afcaf736bfb591dd9
jerbi-ai <jerbimohamedhedi@gmail.com>
2026-01-26 08:32:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,10 @@ initial_accidents = [
      """Il s’agit d’un accident de trajet survenu en raison d’une vitesse excessive du chauffeur 
      et de la présence d’un dos-d’âne non visible en raison des conditions météorologiques défavorables (pluie).
      À la suite de la chute, l’opératrice a subi un traumatisme dorsal.""")
+
+    (datetime.date(2026, 1, 22),
+     "Lors de l’opération de changement de la lame de la machine de coupe, "
+     "le technicien de maintenance s’est blessé au niveau de la main droite.")
 ]
 
 # Ajouter les accidents initiaux dans le calendrier
